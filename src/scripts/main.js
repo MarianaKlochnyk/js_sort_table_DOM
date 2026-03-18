a43c387d8b1193af3c5073ed29ca695713d87034
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-03-18 21:03:13 +0200

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,19 +3,21 @@
 const headers = document.querySelectorAll('th');
 const tbody = document.querySelector('tbody');
 
-headers.forEach((header) => {
+headers.forEach((header, index) => {
   header.addEventListener('click', () => {
-    if (header.textContent === 'Salary') {
-      const rows = Array.from(tbody.querySelectorAll('tr'));
+    const rows = Array.from(tbody.querySelectorAll('tr'));
 
-      const sorted = rows.sort((a, b) => {
-        const aSalary = Number(a.cells[1].textContent);
-        const bSalary = Number(b.cells[1].textContent);
+    const sorted = rows.sort((a, b) => {
+      const aText = a.cells[index].textContent;
+      const bText = b.cells[index].textContent;
 
-        return aSalary - bSalary;
-      });
+      if (!isNaN(aText) && !isNaN(bText)) {
+        return Number(aText) - Number(bText);
+      }
 
-      tbody.append(...sorted);
-    }
+      return aText.localeCompare(bText);
+    });
+
+    tbody.append(...sorted);
   });
 });
