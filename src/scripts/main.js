05fac8f48144e5ca6ba61787966bed5c1d9b5ae7
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-03-18 20:45:33 +0200

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const header = document.querySelector('th');
+const tbody = document.querySelector('tbody');
+
+header.addEventListener('click', () => {
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  const sorted = rows.sort((a, b) => {
+    const aSalary = Number(a.cells[1].textContent);
+    const bSalary = Number(b.cells[1].textContent);
+
+    return aSalary - bSalary;
+  });
+
+  tbody.append(...sorted);
+});
